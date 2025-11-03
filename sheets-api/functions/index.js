e38f57913ec54dc65b59754998bb29f50799d272
“alfonsoj03” <alfonsodjimenez2003@gmail.com>
2025-11-02 20:41:08 -0500

    fix: actualizacion de google services

--- sheets-api/functions/index.js
+++ sheets-api/functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const {setGlobalOptions} = require("firebase-functions/v2");
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
